2ba681f36140e9f246805802ad963e8fd82008a7
vester2012 <30416299+vester2012@users.noreply.github.com>
2025-02-17 17:39:53 +0300

    update

--- webpack/prod.js
+++ webpack/prod.js
@@ -2,26 +2,41 @@ const merge = require("webpack-merge");
 const path = require("path");
 const base = require("./base");
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(base, {
-  mode: "production",
   output: {
-    filename: "bundle.min.js"
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"), // Папка назначения для сборки
   },
   devtool: false,
   performance: {
     maxEntrypointSize: 900000,
-    maxAssetSize: 900000
+    maxAssetSize: 900000,
   },
   optimization: {
+    minimize: true,
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  }
+            comments: false, // Удаляем комментарии
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Очистка папки dist перед сборкой
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "../src"), // Путь к папке src
+          to: path.resolve(__dirname, "../dist/src"), // Целевая папка
+        },
+      ],
+    }),
+  ],
+  mode: "production",
 });
